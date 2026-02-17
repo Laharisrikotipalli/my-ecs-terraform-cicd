4e9ff112a2f67ff83da2631dec373fcd512eabe0
Laharisrikotipalli <laharisrikotipalli07@gmail.com>
2026-02-17 20:55:29 +0530

    Test auto deployment

--- app/index.js
+++ app/index.js
@@ -4,7 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from AWS ECS ");
+  res.send("Hello from AWS ECS - Updated 🚀");
+
 });
 
 app.get("/health", (req, res) => {
